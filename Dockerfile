a5c98319fbd270e4367c046ffb28a383948c5a41
wulff007 <wulff007@gmail.com>
2023-03-03 00:08:21 +0300

    fixing docker

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,13 @@ COPY . .
 
 RUN npm run build
 
-EXPOSE 3000
+ENV DB_HOST=localhost
+ENV DB_PORT=5433
+ENV DB_PASSWORD=password
+ENV DB_USER=user
+ENV DB_DATABASE=tabbled
+ENV PORT=3000
+
+EXPOSE $PORT
+
+CMD ["/bin/sh", "./entrypoint.sh"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { ConfigItem, ConfigRevision } from "./config/entities/config.entity";
         TypeOrmModule.forRoot({
             type: 'postgres',
             host: process.env.DB_HOST,
-            port: Number(process.env.DB_POST),
+            port: Number(process.env.DB_PORT),
             username: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
