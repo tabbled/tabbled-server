c7cf902c6b73ef0579a8615f6e81b8d3de9666ed
wulff007 <wulff007@gmail.com>
2023-04-21 09:25:22 +0300

    version update

--- src/users/users.gateway.ts
+++ src/users/users.gateway.ts
@@ -22,6 +22,7 @@ export class UsersGateway {
 
     @SubscribeMessage('users/me')
     async me(@ConnectedSocket() client: Socket) {
+        console.log('users/me', client['userId'])
 
         try {
 
